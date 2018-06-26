70e0afe9951b243d574c2a21ed7486b7ad4d3bc4
Krutonium <PFCKrutonium@gmail.com>
2018-06-26 01:31:49 -0400

    Dat Commit Doe.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+namespace TransferSH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if(args.Length == 1 == false){
+                Console.WriteLine("Specify a single file!");
+            }
+            else
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(new HttpMethod("PUT"), "https://transfer.sh/"+ args[0]))
+                    {
+                        request.Content = new ByteArrayContent(File.ReadAllBytes(args[0])); 
+                        var response = httpClient.SendAsync(request).Result;
+                        Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                    }
+                }
+            }
+        }
+    }
+}
